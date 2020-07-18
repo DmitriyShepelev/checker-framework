4c44937aa605b7eee510b0f58f43bbbac2a118f7
Dmitriy Shepelev <ph.ds@outlook.com>
2020-07-18 12:12:13 -0700

    Complete CharCast.java test

--- checker/tests/signedness/CharCast.java
+++ checker/tests/signedness/CharCast.java
@@ -1,11 +1,52 @@
+// @skip-test
 public class CharCast {
     int i;
-    void cast() {
-        int x = returnShort();
+
+    void clientReturnedCastedShort() {
+        int x = returnCastedShort();
+        // Subsequent line incorrectly emits argument.type.incompatible error.
         String s = Character.toString((char) x);
     }
 
-    short returnShort() {
+    short returnCastedShort() {
         return (short) i;
+    }
+
+    void clientCastShort() {
+        int x = (short) i;
+        // Subsequent line incorrectly emits argument.type.incompatible error.
+        String s = Character.toString((char) x);
+    }
+
+    void clientReturnedCastedShortLiteral() {
+        int x = returnCastedShortLiteral();
+        // Subsequent line incorrectly emits argument.type.incompatible error.
+        String s = Character.toString((char) x);
+    }
+
+    short returnCastedShortLiteral() {
+        return (short) 1;
+    }
+
+    void clientCastShortLiteral() {
+        int x = (short) 1;
+        String s = Character.toString((char) x);
+    }
+
+    void clientShortUpcast() {
+        short x = 1;
+        int y = x;
+        Character.toString((char) y);
+    }
+
+    void ClientReturnedShortUpcast() {
+        int y = returnShort();
+        // Subsequent line incorrectly emits argument.type.incompatible error.
+        Character.toString((char) y);
+    }
+
+    short returnShort() {
+        short x = 1;
+        return x;
     }
 }
