a23f24a650b2a8828e1e01ddc540f6014a86b4bd
Dmitriy Shepelev <ph.ds@outlook.com>
2020-08-18 16:22:39 -0700

    Add additional tests to UnsignedRightShiftTest.java

--- checker/tests/signedness/UnsignedRightShiftTest.java
+++ checker/tests/signedness/UnsignedRightShiftTest.java
@@ -7,11 +7,18 @@ public class UnsignedRightShiftTest {
         byteArray[0] = (byte) (length >>> 24);
         byteArray[1] = (byte) (length >>> 16);
         byteArray[2] = (byte) (length >>> 8);
-        byteArray[3] = (byte) length;
+        //        byteArray[3] = (byte) length;
     }
 
     void testWrite64(long x) {
         write32((int) (x >>> 32));
+    }
+
+    void testWrite64() {
+        //        long myLong = Long.MAX_VALUE;
+        //        int z = (int) (myLong >>> 32);
+        //        int myInt = 2;
+        //        short w = (short) (myInt >>> 16);
     }
 
     int return12() {
