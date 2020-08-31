77a6582287c5698081685c48ed80d0f42959ba07
Dmitriy Shepelev <ph.ds@outlook.com>
2020-08-30 20:28:45 -0700

    Add CharComparisonsRuntime.java test

---
+++ checker/tests/signedness/src/CharComparisonsRuntime.java
@@ -0,0 +1,38 @@
+public class CharComparisonsRuntime {
+    static void unsignedComparisonLiteralsPrinter(char c, byte b) {
+        boolean res = c > (b & 0xff);
+        System.out.println(res);
+        res = c >= (b & 0xff);
+        System.out.println(res);
+        res = c < (b & 0xff);
+        System.out.println(res);
+        res = c == (b & 0xff);
+        System.out.println(res);
+    }
+
+    static void unsignedComparisonLiterals() {
+        // Negative byte b and char c such that 0 <= c <= 65.
+        char c = 0;
+        byte b = -1;
+        unsignedComparisonLiteralsPrinter(c, b);
+
+        // Negative byte b and char c such that 65 < c.
+        c = 67;
+        b = -1;
+        unsignedComparisonLiteralsPrinter(c, b);
+
+        // Positive byte b and char c such that 65 < c.
+        c = 67;
+        b = 1;
+        unsignedComparisonLiteralsPrinter(c, b);
+
+        // Positive byte b and char c such that 0 <= c <= 65.
+        c = 0;
+        b = 1;
+        unsignedComparisonLiteralsPrinter(c, b);
+    }
+
+    public static void main(String[] args) {
+        unsignedComparisonLiterals();
+    }
+}
