276e815d700ebb65c3e69aa9b2f0b7f4e0cd38ab
Dmitriy Shepelev <ph.ds@outlook.com>
2020-09-13 16:55:19 -0700

    Fix CharComparisonsRuntime.java test

--- checker/tests/signedness/src/CharComparisonsRuntime.java
+++ checker/tests/signedness/src/CharComparisonsRuntime.java
@@ -1,4 +1,9 @@
+import org.checkerframework.checker.signedness.qual.Unsigned;
+
 public class CharComparisonsRuntime {
+    static @Unsigned byte b;
+    static char c;
+
     static void unsignedComparisonLiteralsPrinter(char c, byte b) {
         boolean res = c > b;
         System.out.println(res);
@@ -6,29 +11,31 @@ public class CharComparisonsRuntime {
         System.out.println(res);
         res = c < b;
         System.out.println(res);
+        res = c <= b;
+        System.out.println(res);
         res = c == b;
         System.out.println(res);
     }
 
     static void unsignedComparisonLiterals() {
         // Negative byte b and char c such that 0 <= c <= 65.
-        char c = 0;
-        byte b = -1;
+        c = 0; // unsigned: 0
+        b = -1; // unsigned: 255, signed: -1
         unsignedComparisonLiteralsPrinter(c, b);
 
         // Negative byte b and char c such that 65 < c.
-        c = 67;
-        b = -1;
+        c = 67; // unsigned: 67
+        b = -1; // unsigned: 255, signed: -1
         unsignedComparisonLiteralsPrinter(c, b);
 
         // Positive byte b and char c such that 65 < c.
-        c = 67;
-        b = 1;
+        c = 67; // unsigned: 67
+        b = 1; // unsigned: 1, signed: 1
         unsignedComparisonLiteralsPrinter(c, b);
 
         // Positive byte b and char c such that 0 <= c <= 65.
-        c = 0;
-        b = 1;
+        c = 0; // unsigned: 0
+        b = 1; // unsigned: 1, signed: 1
         unsignedComparisonLiteralsPrinter(c, b);
     }
 
