8aa062a5c4d8c3a429dfe4ba9916ace01dfd93d7
Dmitriy Shepelev <ph.ds@outlook.com>
2020-09-17 17:16:35 -0700

    Add test case for Issue #3669

---
+++ checker/tests/signedness/CompareChars.java
@@ -0,0 +1,13 @@
+// Test case for issue 3668:
+// https://github.com/typetools/checker-framework/issues/3669
+
+// @skip-test
+public class CompareChars {
+    void compareUnsignedChars(char c2) {
+        char c1 = 'a';
+        boolean res = c1 > c2;
+        res = c1 >= c2;
+        res = c1 < c2;
+        res = c1 <= c2;
+    }
+}
