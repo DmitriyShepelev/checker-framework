7fecaf966e4ee1af6796019cf4c4a9369f755591
Michael Ernst <mernst@cs.washington.edu>
2020-09-18 11:28:34 -0700

    Improve test program

--- checker/tests/signedness/src/CastType.java
+++ checker/tests/signedness/src/CastType.java
@@ -1,22 +1,38 @@
 // @skip-test
 import org.checkerframework.checker.signedness.qual.Unsigned;
 
+// The output of this program is:
+
+// signed: 2147483647 => 127
+// unsigned: 2147483647 => 127
+// signed: -2147483648 => -128
+// unsigned: 2147483648 => 4294967168
+// signed: 0 => 0
+// unsigned: 0 => 0
+// signed: 1 => 0
+// unsigned: 1 => 0
+// signed: -1 => -1
+// unsigned: 4294967295 => 4294967295
+// signed: 100 => 0
+// unsigned: 100 => 0
+
 public class CastType {
-    static void testUnsignedCastType(@Unsigned int length) {
-        byte b1 = (@Unsigned byte) (length >>> 24);
-        System.out.println(b1);
+    static void testUnsignedCastType(@Unsigned int input) {
+        byte output = (@Unsigned byte) (input >>> 24);
+        System.out.println("signed: " + input + " => " + output);
+        System.out.println("unsigned: " + intUnsigned(input) + " => " + intUnsigned(output));
+    }
+
+    static long intUnsigned(int i) {
+        return i & 0xFFFFFFFFL;
     }
 
     public static void main(String[] args) {
-        testUnsignedCastType(Integer.MAX_VALUE); // unsigned: 2^32 - 1, signed: -1, shifted
-        // signed: -1, shifted unsigned: 255
-        testUnsignedCastType(Integer.MIN_VALUE); // unsigned: 2^31, signed: -2^31, shifted
-        // signed: -128, shifted unsigned: 128
-        testUnsignedCastType(0); // unsigned: 0, signed: 0
-        testUnsignedCastType(1); // unsigned: 1, signed: 1
-        testUnsignedCastType(-1); // unsigned: 2^32 - 1, signed: -1, shifted signed: -1,
-        // shifted unsigned: 255
-        testUnsignedCastType(100); // unsigned: 100, signed: 100, shifted unsigned: 0, shifted
-        // unsigned: 0
+        testUnsignedCastType(Integer.MAX_VALUE);
+        testUnsignedCastType(Integer.MIN_VALUE);
+        testUnsignedCastType(0);
+        testUnsignedCastType(1);
+        testUnsignedCastType(-1);
+        testUnsignedCastType(100);
     }
 }
