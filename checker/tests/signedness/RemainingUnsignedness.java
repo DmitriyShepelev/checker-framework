96d3acb9b6f33c3868a48ba536f7bf55dba26822
Dmitriy Shepelev <ph.ds@outlook.com>
2020-09-28 20:18:12 -0700

    Add RemainingUnsignedness test

---
+++ checker/tests/signedness/RemainingUnsignedness.java
@@ -0,0 +1,19 @@
+public class RemainingUnsignedness {
+    int returnIntWithLocalVariable(char c) {
+        int i = c;
+        return i;
+    }
+
+    long returnLongWithLocalVariable(char c) {
+        long l = c;
+        return l;
+    }
+
+    int returnIntWithoutLocalVariable(char c) {
+        return c;
+    }
+
+    long returnLongWithoutLocalVariable(char c) {
+        return c;
+    }
+}
