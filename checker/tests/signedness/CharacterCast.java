fe4ae899d3501d3e6a55a5495241e1cb2df759cd
Dmitriy Shepelev <ph.ds@outlook.com>
2020-09-28 21:28:08 -0700

    Add CharacterCast test

---
+++ checker/tests/signedness/CharacterCast.java
@@ -0,0 +1,10 @@
+public class CharacterCast {
+    void castCharacter(Object o) {
+        floatParameter((Character) o);
+        doubleParameter((Character) o);
+    }
+
+    void floatParameter(float f) {}
+
+    void doubleParameter(double d) {}
+}
